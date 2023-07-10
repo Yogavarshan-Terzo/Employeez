e6f8ae70c6506080eed765a7774d35277748d8a0
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-07-10 16:10:32 +0530

    1. Initialize Backend

---
+++ src/test/java/com/terzocloud/employeez/EmployeezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terzocloud.employeez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
